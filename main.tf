af571b9a2e614d140a874f1018912ff55303c66d
Micky Hallabrin <misaiahh@proton.me>
2025-05-20 10:12:16 -0400

    added description

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_ssm_parameter" "example_parameter" {
   name  = var.example_parameter_name
   value = var.example_parameter_value
+  description = "An example parameter created using Terraform"
   type = "String"
   tags = {
     Environment = "Terraform"
