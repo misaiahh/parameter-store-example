1ad2f009b877563b42fc443f407aabb11397e212
Micky Hallabrin <misaiahh@proton.me>
2025-05-20 10:39:05 -0400

    added blank aws variables

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_ssm_parameter" "example_parameter" {
-  name  = var.example_parameter_name
-  value = var.example_parameter_value
+  name        = var.example_parameter_name
+  value       = var.example_parameter_value
   description = "An example parameter created using Terraform"
-  type = "String"
+  type        = "String"
   tags = {
     Environment = "Terraform"
     Project     = "Parameter Store Example"
--- variables.tf
+++ variables.tf
@@ -15,3 +15,13 @@ variable "example_parameter_value" {
   type        = string
   default     = "example_value"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS access key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key"
+  type        = string
+}
